996cd568a42332b51373957418fd26b83d993538
Nar0118 <khachatryannarek42@gmail.com>
2024-06-16 12:07:02 +0400

    merged with three.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,99 +1,113 @@
 export const menuItem = [
-    { title: "About Me", path: 'about'},
-    { title: "Technical Skills", path: 'skills'},
-    { title: "My Projects", path: 'projects'},
-    { title: "Contact Me", path: 'contact'},
+  { title: "About Me", path: "about" },
+  { title: "Technical Skills", path: "skills" },
+  { title: "My Projects", path: "projects" },
+  { title: "Contact Me", path: "contact" },
 ];
 
 export const skillsFrontEnd = [
-    { title: "React.js", src: 'icons/reactJs.svg' },
-    { title: "Angular", src: 'icons/angular.svg' },
-    { title: "Vue.js", src: 'icons/vue.svg' },
-    { title: "React Native", src: 'icons/react-native.svg' },
+  { title: "React.js", src: "icons/reactJs.svg" },
+  { title: "Angular", src: "icons/angular.svg" },
+  { title: "Vue.js", src: "icons/vue.svg" },
+  { title: "React Native", src: "icons/react-native.svg" },
 ];
 
 export const skillsBackEnd = [
-    { title: "Node.js", src: 'icons/node.svg' },
-    { title: "REST API", src: 'icons/rest.svg' },
-    { title: "GraphQL", src: 'icons/graphQl.svg' },
-    { title: "ASP.NET Core", src: 'icons/asp.svg' },
-    { title: "Laravel", src: 'icons/laravel-icon.png' },
-]; 
+  { title: "Node.js", src: "icons/node.svg" },
+  { title: "REST API", src: "icons/rest.svg" },
+  { title: "GraphQL", src: "icons/graphQl.svg" },
+  { title: "ASP.NET Core", src: "icons/asp.svg" },
+  { title: "Laravel", src: "icons/laravel-icon.png" },
+];
 
 export const skillsDatabase = [
-    { title: "MySQL", src: 'icons/mysql.svg' },
-    { title: "PostgreSQL", src: 'icons/postgres.svg' },
-    { title: "Mongo DB", src: 'icons/mongo.svg' },
+  { title: "MySQL", src: "icons/mysql.svg" },
+  { title: "PostgreSQL", src: "icons/postgres.svg" },
+  { title: "Mongo DB", src: "icons/mongo.svg" },
 ];
 
 export const skills = [
-    { skill: "FE", src: 'images/FE-dev.svg' },
-    { skill: "BE", src: 'images/BE-dev.svg' },
-    { skill: "DB", src: 'images/DB-dev.svg' },
+  { skill: "FE", src: "images/FE-dev.svg" },
+  { skill: "BE", src: "images/BE-dev.svg" },
+  { skill: "DB", src: "images/DB-dev.svg" },
 ];
 
 export const projectDetails = [
-    {
-        title: "Forbes",
-        about: "",
-        description: "Forbes is an American business magazine founded by B. C. Forbes in 1917 and owned by Hong Kong-based investment group Integrated Whale Media Investments since 2014. Its chairperson and editor-in-chief is Steve Forbes, and its CEO is Mike Federle. It is based in Jersey City, New Jersey. Competitors in the national business magazine category include Fortune and Bloomberg Businessweek.",
-        challenges: "Added new features. Bug-fixing on the front-end, which I conducted, was the challenging aspect of working on a project on Personalized Home Health Tests and Professional Coaching for Optimal Health.",
-        role: "Full-Stack Developer",
-        websiteHref: "https://www.forbes.com/",
-        src: "images/projects/forbes.png"
-    },
-    {
-        title: "Ethermail",
-        about: "Venture Capital Re-Created for the Masses",
-        description: "For anonymous and secure wallet-to-wallet communication, EtherMail is the leading email solution. EtherMail compensates users for reading pertinent content in their inboxes and enables completely anonymous and encrypted P2P communication.",
-        challenges: "To have a Web3-based email service. I contributed to the project's maintenance and the creation of brand-new mobile apps. The product is in use and already has actual users.",
-        role: "Full-Stack Developer",
-        websiteHref: "https://ethermail.io/",
-        src: "images/projects/ethermail.jpg"
-    },
-    {
-        title: "NordicVelo",
-        about: "Altcoins, Market Making, and Trading",
-        description: "Nordic Velo is a high-frequency trading firm that provides Market Making services to some of the world's most promising cryptocurrency projects on both centralized and decentralized exchanges. Offers - exchange listing, market-making, execution, venture fund, and consulting services.",
-        challenges: "I was working on the front end to construct pages with charts that displayed data retrieved from Firebase. Also, testing the functioning meets the requirements of the client or end-user.",
-        role: "Front-end Developer",
-        websiteHref: "https://nordicvelo.com/",
-        src: "images/projects/nordicvelo.jpg"
-    },
-    {
-        title: "Cool Cats",
-        about: "Remember, all Cats are cool",
-        description: "Cool Cats are a collection of programmatically random-generated NFTs on the Ethereum blockchain. The 1st generation consists of 9,999 randomly assembled Cats from over 300,000 total options. Cool Cats have a variety of outfits, faces, and colors. Each Cool Cat consists of a unique body, hat, face, and outfit–the possibilities are virtually endless.",
-        challenges: "Creating APIs for the front end is the biggest challenge to displaying blockchain data until working on the back end and all at once developing smart contracts for all games and quests.",
-        role: "Back-End Engineer",
-        websiteHref: "https://www.coolcatsnft.com/",
-        src: "images/projects/coolcat.jpg"
-    },
-    {
-        title: "CryptoPool",
-        about: "",
-        description: "CryptoPool is a tokenomics-based On-Off Ramp Solution that allows users to buy and sell cryptocurrencies directly while supporting several tokens for DEXs, NFTs & marketplaces on major chains.",
-        challenges: "Added new features. Bug-fixing on the front-end, which I conducted, was the challenging aspect of working on a project on Personalized Home Health Tests and Professional Coaching for Optimal Health.",
-        role: "Frontend Developer",
-        websiteHref: "https://www.cryptopool.money/",
-        src: "images/projects/crypto-pool.webp"
-    },
-    {
-        title: "Hearst",
-        about: "Personalized Home Health Tests for Your Optimal Health",
-        description: "Hearst is an innovative platform that revolutionizes the concept of mining servers by bridging the gap between server owners and users seeking computing power. It serves as a decentralized marketplace where clients can offer their mining servers for rent, while users have the opportunity to utilize these servers for various purposes in exchange for monetary compensation.",
-        challenges: "Added new features. Bug-fixing on the front-end and back-end, which I conducted, was the challenging aspect of working on a project on Personalized Home Health Tests and Professional Coaching for Optimal Health.",
-        role: "Full-Stack Developer",
-        websiteHref: "https://hearst-corporation-fe.vercel.app/",
-        src: "images/projects/hearst.webp"
-    },
-    {
-        title: "Recrout",
-        about: "",
-        description: "Recrout's innovative HR technology helps you automate the labor-intensive application and selection process. No more cumbersome, repetitive tasks, such as endless CV and cover letter screening. It's the easiest-to-use and competency-driven recruitment system that helps you predict the best matches based on soft skills.",
-        challenges: "Added new features. Worked on old features. Made new version of website from scratch.",
-        role: "Frontend Developer",
-        websiteHref: "https://recrout.com/",
-        src: "images/projects/recrout.webp"
-    },
+  {
+    title: "Forbes",
+    about: "",
+    description:
+      "Forbes is an American business magazine founded by B. C. Forbes in 1917 and owned by Hong Kong-based investment group Integrated Whale Media Investments since 2014. Its chairperson and editor-in-chief is Steve Forbes, and its CEO is Mike Federle. It is based in Jersey City, New Jersey. Competitors in the national business magazine category include Fortune and Bloomberg Businessweek.",
+    challenges:
+      "Added new features. Bug-fixing on the front-end, which I conducted, was the challenging aspect of working on a project on Personalized Home Health Tests and Professional Coaching for Optimal Health.",
+    role: "Full-Stack Developer",
+    websiteHref: "https://www.forbes.com/",
+    src: "images/projects/forbes.png",
+  },
+  {
+    title: "Ethermail",
+    about: "Venture Capital Re-Created for the Masses",
+    description:
+      "For anonymous and secure wallet-to-wallet communication, EtherMail is the leading email solution. EtherMail compensates users for reading pertinent content in their inboxes and enables completely anonymous and encrypted P2P communication.",
+    challenges:
+      "To have a Web3-based email service. I contributed to the project's maintenance and the creation of brand-new mobile apps. The product is in use and already has actual users.",
+    role: "Full-Stack Developer",
+    websiteHref: "https://ethermail.io/",
+    src: "images/projects/ethermail.jpg",
+  },
+  {
+    title: "NordicVelo",
+    about: "Altcoins, Market Making, and Trading",
+    description:
+      "Nordic Velo is a high-frequency trading firm that provides Market Making services to some of the world's most promising cryptocurrency projects on both centralized and decentralized exchanges. Offers - exchange listing, market-making, execution, venture fund, and consulting services.",
+    challenges:
+      "I was working on the front end to construct pages with charts that displayed data retrieved from Firebase. Also, testing the functioning meets the requirements of the client or end-user.",
+    role: "Front-end Developer",
+    websiteHref: "https://nordicvelo.com/",
+    src: "images/projects/nordicvelo.jpg",
+  },
+  {
+    title: "Cool Cats",
+    about: "Remember, all Cats are cool",
+    description:
+      "Cool Cats are a collection of programmatically random-generated NFTs on the Ethereum blockchain. The 1st generation consists of 9,999 randomly assembled Cats from over 300,000 total options. Cool Cats have a variety of outfits, faces, and colors. Each Cool Cat consists of a unique body, hat, face, and outfit–the possibilities are virtually endless.",
+    challenges:
+      "Creating APIs for the front end is the biggest challenge to displaying blockchain data until working on the back end and all at once developing smart contracts for all games and quests.",
+    role: "Back-End Engineer",
+    websiteHref: "https://www.coolcatsnft.com/",
+    src: "images/projects/coolcat.jpg",
+  },
+  {
+    title: "CryptoPool",
+    about: "",
+    description:
+      "CryptoPool is a tokenomics-based On-Off Ramp Solution that allows users to buy and sell cryptocurrencies directly while supporting several tokens for DEXs, NFTs & marketplaces on major chains.",
+    challenges:
+      "Added new features. Bug-fixing on the front-end, which I conducted, was the challenging aspect of working on a project on Personalized Home Health Tests and Professional Coaching for Optimal Health.",
+    role: "Frontend Developer",
+    websiteHref: "https://www.cryptopool.money/",
+    src: "images/projects/crypto-pool.webp",
+  },
+  {
+    title: "Hearst",
+    about: "Personalized Home Health Tests for Your Optimal Health",
+    description:
+      "Hearst is an innovative platform that revolutionizes the concept of mining servers by bridging the gap between server owners and users seeking computing power. It serves as a decentralized marketplace where clients can offer their mining servers for rent, while users have the opportunity to utilize these servers for various purposes in exchange for monetary compensation.",
+    challenges:
+      "Added new features. Bug-fixing on the front-end and back-end, which I conducted, was the challenging aspect of working on a project on Personalized Home Health Tests and Professional Coaching for Optimal Health.",
+    role: "Full-Stack Developer",
+    websiteHref: "https://hearst-corporation-fe.vercel.app/",
+    src: "images/projects/hearst.webp",
+  },
+  {
+    title: "Recrout",
+    about: "",
+    description:
+      "Recrout's innovative HR technology helps you automate the labor-intensive application and selection process. No more cumbersome, repetitive tasks, such as endless CV and cover letter screening. It's the easiest-to-use and competency-driven recruitment system that helps you predict the best matches based on soft skills.",
+    challenges:
+      "Added new features. Worked on old features. Made new version of website from scratch.",
+    role: "Frontend Developer",
+    websiteHref: "https://recrout.com/",
+    src: "images/projects/recrout.webp",
+  },
 ];
