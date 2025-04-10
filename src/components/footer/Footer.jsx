ebca4c0121548f7c1d4733cba75238092d384697
Nar0118 <khachatryannarek42@gmail.com>
2025-04-10 14:41:22 +0400

    deploy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,8 @@
 import styles from './footer.module.scss';
 
-export const Footer = () => 
-    <section className={styles.footer}>
-      <p className={styles.footer_p}>
-        © {new Date().getFullYear()}. All rights reserved.
-      </p>
-    </section>
+export const Footer = () =>
+  <section className={styles.footer}>
+    <p className={styles.footer_p}>
+      © {new Date().getFullYear()}. All rights reserved.
+    </p>
+  </section>
